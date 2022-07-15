76cc849baeb1b40629642562226ab8f7da28c6ec
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-15 19:35:53 +0800

    Initial Commit

---
+++ OrderBelowBugVentura/OrderBelowBugVenturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderBelowBugVenturaApp.swift
+//  OrderBelowBugVentura
+//
+//  Created by Guillaume Leclerc on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct OrderBelowBugVenturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
